dd4c9d2dce2dd229b38d4fc861a7cc4f4c605979
GabrielMüllerDS <61920654+GabrielMullerDS@users.noreply.github.com>
2022-03-15 01:33:07 -0300

    Create index.js

---
+++ Aula2/index.js
@@ -0,0 +1,14 @@
+// const fs = require('fs'); formato antigo de fazer um import
+import {promises as fs } from 'fs';
+async function readData() {
+  try{//caso tentar esse codigo porem não der certo
+  const data = JSON.parse(await fs.readFile("./people.json"));
+  return data;
+  } catch(err) {//ele vai apresentar o erro de forma mais simples.
+    console.log('Erro na leitura do arquivo ;-;');
+  }
+}
+function doMap() {
+  const people = readData();
+}
+readData();
